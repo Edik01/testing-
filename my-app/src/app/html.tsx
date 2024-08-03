66fd7d0b7ef28ddfb9bf516c6521904fc7715c7d
Edik Huloian <guloyan.edik@gmail.com>
2024-08-03 08:50:22 -0700

    update

--- my-app/src/app/html.tsx
+++ my-app/src/app/html.tsx
@@ -1,8 +1,13 @@
-async function fetchData(): Promise<void> {
+async function fetchData(): Promise<void>{
   try {
-    const response =await fetch ('htpps://api.axample.com/data');
-    if (response.ok) {
-      
+    const response = await fetch('https://api.example.com/data');
+    if (!response.ok){
+      throw new Error(`HTTP error! status: ${response.status}`);
+      console.log(data);
+    } catch (error){
+      console.error('error fetching data:', error);
     }
   }
-}
+
+
+fetchData();
