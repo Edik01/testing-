332185ff01e09a03bc8cac4cea17ceb1ad01e1b6
Edik Huloian <guloyan.edik@gmail.com>
2024-08-02 17:59:26 -0700

    update

--- my-app/src/app/html.tsx
+++ my-app/src/app/html.tsx
@@ -1,36 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
-const Home = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+async function fetchData(): Promise<void> {
+  try {
+    const response =await fetch ('htpps://api.axample.com/data');
+    if (response.ok) {
+      
+    }
+  }
 }
-
-export default App;
